f7a6bf356b23b1bffd78f3b852ce63270e27ced1
Gabriel Ramalho <joaogabriel.ejn@gmail.com>
2026-09-22 19:26:47 -0300

    feat: add Books Entity with attributes

---
+++ src/main/java/com/ramalho/library/database/model/BooksEntity.java
@@ -0,0 +1,42 @@
+package com.ramalho.library.database.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BooksEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String Gender;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "Author", nullable = false)
+    private AuthorEntity Author;
+
+    @Column(nullable = false)
+    private String edition;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "Publisher", nullable = false)
+    private PublisherEntity Publusher;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime registrationDate;
+}
